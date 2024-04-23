fbe4cd23bdab85dfc21a37af73421962a86625ac
maur32 <maur.martins32@gmail.com>
2024-04-23 19:23:06 -0300

    Fixing JWT

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -4,9 +4,13 @@ from rest_framework import serializers
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["id", "username", "password"]
+        fields = ("username", "password")
         extra_kwargs = {"password":{"write_only": True}}
 
-        def create(self, validated_data):
-            user = User.objects.create_user(**validated_data)
-            return user
+    def create(self, validated_data):
+        user = User(
+            username= validated_data['username']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
